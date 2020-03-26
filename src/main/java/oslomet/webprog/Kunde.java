cce27efa74abf344e0cb707d396ff9494c85b424
sarime <sarime@student.matnat.ifi.no>
2020-03-26 19:25:04 +0100

    SpringBoot GET/POST, save data in array in the Server

---
+++ src/main/java/oslomet/webprog/Kunde.java
@@ -0,0 +1,27 @@
+package oslomet.webprog;
+
+public class Kunde {
+    private String navn;
+    private String adresse;
+    public Kunde(String navn, String adresse){
+        this.navn = navn;
+        this.adresse = adresse;
+    }
+    public Kunde(){}
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+}
---
+++ src/main/java/oslomet/webprog/KundeController.java
@@ -0,0 +1,22 @@
+package oslomet.webprog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class KundeController {
+    private  final List<Kunde> alleKunder = new ArrayList<>();
+
+    @PostMapping("/lagre")
+    public void lagreKunde(Kunde innKunde){
+        alleKunder.add(innKunde);
+    }
+    @GetMapping("/hentAlle")
+    public List<Kunde> hentAlle(){
+        return alleKunder;
+    }
+}
